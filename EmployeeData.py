d6c66b56e9db29b70baa938f784d30535415ad50
Mitali Soni <mitali.soni@csgi.com>
2022-05-27 08:10:38 +0530

    Initial Commit

---
+++ EmployeeData.py
@@ -0,0 +1,37 @@
+import csv
+from datetime import date
+
+
+def load_employee_data():
+    """Saving a csv file as a list of dictionaries"""
+    csv_file = open("/Users/sonmit01/Documents/Data/empData.csv")
+    csv_reader = csv.reader(csv_file)
+    header = []
+    header = next(csv_reader)
+    list_data = []
+    for row in csv_reader:
+        current_row = []
+        current_row = row
+        dict_row = {}
+        for i in range(len(header)):
+            dict_row[header[i]] = current_row[i]
+        list_data.append(dict_row)
+    print(list_data)
+    return list_data
+
+
+def add_employee(name):
+    csv_file = open("/Users/sonmit01/Documents/Data/empData.csv", mode='r+', encoding='UTF8', newline='')
+    csv_reader = csv.reader(csv_file)
+    header = []
+    header = next(csv_reader)
+    today = date.today()
+    today = today.strftime("%d/%m/%Y")
+    record = {}
+    emp_id = len(list(csv_reader)) + 1
+    csvwriter = csv.DictWriter(csv_file, header)
+    record[header[0]] = emp_id
+    record[header[1]] = name
+    record[header[2]] = today
+    print(record)
+    csvwriter.writerow(record)
---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import EmployeeData as ed
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #ed.add_employee("Mitali Soni")
+    ed.load_employee_data()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
